8fe9a6f0bd942f63687c6b2b6d0c3516eca34420
Caroline <carolinenolasco@MacBook-Air-3.home>
2019-08-02 10:09:08 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,20 +28,34 @@ var cardsInPlay = [];
 var checkForMatch = function() {
 	if(cardsInPlay.length === 2) {
 		if(cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for( var i = 0; i < cards.length; i++) {
+		// cards flip logic goes here
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute( "src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+} 
+
+createBoard();
+
